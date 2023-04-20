7eea07c0f19552e95c07bfd7c2a9d3a994a6f1d8
Sainty93 <jenandfloyd93@icloud.com>
2023-04-20 13:32:41 -0700

    went back and chnages resolver to resolve error

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,70 +4,71 @@ const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
     Query: {
-        me: async (parents, args, context)  => {
-            if (context.user) {
-                const foundUser = await User.findOne({_id: context.user._id}).select('-password');
-                return foundUser;
+        users: async () => {
+            return User.find({}).populate('savedBooks');
+        },       
+        user: async (parent, { id, username }, context) => {
+            const foundUser =  await User.findOne({
+              $or: [{ _id: context.user ? context.user._id : id }, { username: username }],
+            });
+            if (!foundUser) {
+              throw new AuthenticationError('Cannot find a user with this id!');
             }
-            throw new AuthenticationError('User not logged in');
+            return foundUser;
         },
     },
-
     Mutation: {
         addUser: async (parent, { username, email, password }) => {
             const user = await User.create({ username, email, password });
             const token = signToken(user);
 
-            return { user, token };
-    },
-    login: async (parent, { email, password }) => {
-        const user =    await User.findOne({ email, password });
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+              throw new AuthenticationError('No user found with this email address');
+            }
 
-        if (!user) {
-            throw new AuthenticationError('Incorrect Email');
-        }
+            const correctPw = await user.isCorrectPassword(password);
 
-        const correctPassword = await user.isCorrectPassword(password);
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
 
-        if (!correctPassword) {
-            throw new AuthenticationError('Incorrect Password');
-        }
+            const token = signToken(user);
 
-        const token = signToken(user);
-        return { user, token };
-    },
-    saveBook: async (parent, { bookData }, context) => {
-        if (context.user) {
-            const updateUser = await User.findbyOneAndUpdate(
-                { _id: context.user._id },
-                { $addToSet: { savedBooks: bookData } },
+            return { token, user };
+        },
+        saveBook: async (parent, { _id, args }) => {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: args.id },
+                { $addToSet: {
+                    savedBooks: {
+                      authors: args.authors,
+                      description: args.description,
+                      bookId: args.bookId,
+                      image: args.image,
+                      link: args.link,
+                      title: args.title,
+                    },
+                  },
+                },
+                { new: true, runValidators: true }
+              );
+
+              return updatedUser;
+        },
+        deleteBook: async (parent, { _id, bookId }) => {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id },
+                { $pull: { savedBooks: { bookId: bookId } } },
                 { new: true }
-            );
+              );
 
-            return updateUser;
-        }
-
-        throw new AuthenticationError('Please Login to save book.');
-    },
-    deleteBook: async (parent, { bookId }, context) => {
-        if (context.user) {
-            const updateUser = await User.findOneAndUpdate(
-            { _id: context.user._id },
-            { $pull: { savedBooks:  bookId } },
-            { new: true }
-            );
-
-};
-
-    return updateUser;
-    }
-
-    throw new AuthenticationError('Please log in to delete book.');
-
-
+              return updatedUser;
         },
     },
 };
 
-module.exports =resolvers;
-
+module.exports = resolvers;
