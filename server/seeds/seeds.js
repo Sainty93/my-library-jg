a8f1647a7fb26b653cfff3f4699f1f56157c9e0a
Sainty93 <jenandfloyd93@icloud.com>
2023-04-20 13:29:09 -0700

    user seed started

--- server/seeds/seeds.js
+++ server/seeds/seeds.js
@@ -1,14 +1,17 @@
 const db = require('../config/connection');
-const { BOOK } = require('../models');
+const { BOOK, User } = require('../models');
 
 const bookData = require('./bookData.json');
+const userData = require('./userData.json');
 
 db.once('open', async () => {
     await Book.deleteMany({});
+    await User.deleteMany({});
 
     const books = await Book.insertmany(booksData);
+    const users = await User.insertmany(usersData);
 
-    console.log('Book seeded!');
+    console.log('Book and users seeded!');
     process.exit(0);
 
 });
