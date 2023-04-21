8ed3fd15373edfa878711768b2eb72964aea9668
Sainty93 <jenandfloyd93@icloud.com>
2023-04-21 09:11:57 -0700

    went back added typedefs it had not saved/made corrections ro savedbook

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React,  { useState, useEffect } from 'react';
 import {
   Container,
   Card,
@@ -7,17 +7,30 @@ import {
   Col
 } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
+
+//import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
+import { useLazyQuery, useMutation } from '@apollo/client';
+import { QUERY_ME } from '../utils/queries';
+import { DELETE_BOOK } from '../utils/mutations';
 
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
+  const [oneUser] = useLazyQuery(QUERY_ME);
+  const [deleteBook] = useMutation(DELETE_BOOK);
+  const url = window.location.href;
+
+
 
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
 
   useEffect(() => {
+    getUserData();
+    console.log('effect')
+  }, [url,userDataLength]);
+
     const getUserData = async () => {
       try {
         const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -26,56 +39,73 @@ const SavedBooks = () => {
           return false;
         }
 
-        const response = await getMe(token);
+        const { data } = Auth.getProfile(token);
+        const user = data;
 
-        if (!response.ok) {
-          throw new Error('something went wrong!');
+          if (!user) {
+            return false;
+          }
+
+          // get current user's saved books
+          const thisUser = await oneUser(
+            {
+              variables: {
+                id: user._id,
+                username: user.username
+              },
+              pollInterval: 500,
+            }
+          );
+          
+          console.log(thisUser.data.oneUser);
+
+          setUserData(thisUser.data.oneUser);
+      } catch (err) {
+        console.error.apply(err);
+      }
+         
+  };
+
+  const handleDeleteBookmark = async (bookId) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+        if (!token) {
+          return false;
         }
+    const { data } = Auth.getProfile(token);
+    const user = data;
 
-        const user = await response.json();
-        setUserData(user);
+    if (!user) {
+      return false;
+    }
+      try {
+        const thisBook = await deleteBook({
+          variables: {
+            id: user_id,
+            bookId: bookId
+          }
+        });
+
+
+    setUserData(thisBook.data.deleteBook);
+    //upon success, remove books id from local storage
+    removeBookId(bookId);
+
       } catch (err) {
         console.error(err);
       }
-    };
-
-    getUserData();
-  }, [userDataLength]);
-
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
-  const handleDeleteBook = async (bookId) => {
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
-
-    try {
-      const response = await deleteBook(bookId, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
-      removeBookId(bookId);
-    } catch (err) {
-      console.error(err);
-    }
   };
 
-  // if data isn't here yet, say so
+  //if data isnt her yet, say it
   if (!userDataLength) {
-    return <h2>LOADING...</h2>;
-  }
+    return <h2>Loading..</h2>;
 
   return (
     <>
-      <div fluid className='text-light bg-dark p-5'>
+      <div className='text-light bg-dark p-5'>
         <Container>
           <h1>Viewing saved books!</h1>
+          <Button className='btn-block' onClick={() => getUserData()}>Refresh</Button>
         </Container>
       </div>
       <Container>
@@ -87,8 +117,8 @@ const SavedBooks = () => {
         <Row>
           {userData.savedBooks.map((book) => {
             return (
-              <Col md="4">
-                <Card key={book.bookId} border='dark'>
+              <Col key={book.bookId} md="4">
+                <Card border='dark'>
                   {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
                   <Card.Body>
                     <Card.Title>{book.title}</Card.Title>
